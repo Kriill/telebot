d4bc2037d13f3b05aa31aaf6ba71180ed3739973
Kirill <kir@artaill.com>
2025-04-13 11:59:03 +0300

    new file:   main.py

--- guess_numberbot.py
+++ guess_numberbot.py
@@ -7,6 +7,13 @@ from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 
 
+#from environs import Env
+#env = Env()  # Создаем экземпляр класса Env
+#env.read_env()  # Методом read_env() читаем файл .env и загружаем из него переменные в окружение                           
+#BOT_TOKEN = env('BOT_TOKEN')
+
+
+
 dotenv.load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
@@ -35,7 +42,7 @@ def get_user(message: Message):
 
 # Функция возвращающая случайное целое число от 1 до 100
 def get_random_number() -> int:
-    return random.randint(1, 100)
+    return random.randint(1, 200)
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -54,7 +61,7 @@ async def process_start_command(message: Message):
 async def process_help_command(message: Message):
     get_user(message)
     await message.answer(
-        f'Правила игры:\n\nЯ загадываю число от 1 до 100, '
+        f'Правила игры:\n\nЯ загадываю число от 1 до 200, '
         f'а вам нужно его угадать\nУ вас есть {ATTEMPTS} '
         f'попыток\n\nДоступные команды:\n/help - правила '
         f'игры и список команд\n/cancel - выйти из игры\n'
@@ -107,13 +114,13 @@ async def process_positive_answer(message: Message):
         get_user(message)['secret_number'] = get_random_number()
         get_user(message)['attempts'] = ATTEMPTS
         await message.answer(
-            'Ура!\n\nЯ загадал число от 1 до 100, '
+            'Ура!\n\nЯ загадал число от 1 до 200, '
             'попробуй угадать!'
         )
     else:
         await message.answer(
             'Пока мы играем в игру я могу '
-            'реагировать только на числа от 1 до 100 '
+            'реагировать только на числа от 1 до 200 '
             'и команды /cancel и /stat'
         )
 
@@ -129,12 +136,12 @@ async def process_negative_answer(message: Message):
     else:
         await message.answer(
             'Мы же сейчас с вами играем. Присылайте, '
-            'пожалуйста, числа от 1 до 100'
+            'пожалуйста, числа от 1 до 200'
         )
 
 
 # Этот хэндлер будет срабатывать на отправку пользователем чисел от 1 до 100
-@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 200)
 async def process_numbers_answer(message: Message):
     if get_user(message)['in_game']:
         if int(message.text) == get_user(message)['secret_number']:
